ec58d9679108b26c643dfdf6fbfe6b163bf0e4a1
Erickson <rixon.code@gmail.com>
2025-09-06 23:06:04 +0800

    fix(backend): fixed na yung sinira ko tangina

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(3000, () => {
+    console.log('I am listening on port 3000')
+})
+
+export default app
