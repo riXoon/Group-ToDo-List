72e2b4b333bda94bf398607d0cf64f4338ed728a
Erickson <rixon.code@gmail.com>
2025-09-07 23:43:55 +0800

    route(user): added routing for user requests

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { getUser, getUsers } from '../controllers/user.controller.js'
+
+
+const userRouter = Router()
+
+userRouter.get('/', getUsers)
+
+userRouter.get('/:id', getUser)
