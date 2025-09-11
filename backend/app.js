4a3b545de387ded17de7df7970915716d5d493f9
Erickson <rixon.code@gmail.com>
2025-09-11 23:38:03 +0800

    feat(app): restructure app.js to include auth router and error middleware

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,25 @@
 import express from 'express'
 import { PORT } from './config/env.js'
 import connectToDatabase from './database/mongodb.js'
+import cookieParser from 'cookie-parser';
 
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.routes.js'
+
+import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express()
 
+app.set('trust proxy', true);
 
-app.use('api/v1/users', userRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+
+app.use(errorMiddleware);
 
 
 app.get('/', (req, res) => {
