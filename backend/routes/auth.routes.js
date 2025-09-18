061edfc9bc5f1417f0da873120affee895f8f26f
Erickson <rixon.code@gmail.com>
2025-09-19 00:16:23 +0800

    feat(auth): implement sign-in functionality with error handling

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router()
 
 authRouter.post('/sign-up', signUp)
-authRouter.post('/sign-in', (req, res) => 'signed in')
+authRouter.post('/sign-in', signIn)
 authRouter.post('/sign-out', (req, res) => 'signed out')
 
 export default authRouter
